12d54c8404d13ec70e3671571aa076303eee7795
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-23 09:35:52 +0300

    Spruce

--- print/src/Example.java
+++ print/src/Example.java
@@ -1,9 +1,10 @@
 public class Example {
     public static void main(String[] args) {
-        System.out.println("   *   ");
-        System.out.println("  ***  ");
-        System.out.println(" ****** ");
-        System.out.println("********");
-        System.out.println("   *   ");
+        System.out.println("    *   ");
+        System.out.println("   ***  ");
+        System.out.println("  ***** ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+        System.out.println("    *   ");
     }
 }
