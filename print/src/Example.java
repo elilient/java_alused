3303d63fd82d7c832b0ac77a96ae3aaf1ea137e0
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-23 09:49:11 +0300

    Calculation using parentheses

--- print/src/Example.java
+++ print/src/Example.java
@@ -1,8 +1,9 @@
 public class Example {
     public static void main(String[] args) {
-        int first = 2;
-        int second = 4;
-        int sum = first + second;
-        System.out.println(sum);
+        int calcWithparens = (1 + 1) + 3 * (2 + 5);
+        int calcWithoutparens = 1 + 1 + 3 * 2 + 5;
+
+        System.out.println(calcWithoutparens);
+        System.out.println(calcWithparens);
     }
 }
