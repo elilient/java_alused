0d0c6dee0bd88ff3ccbc9bb226eec9b79e3baeec
Edgar Lilienthal <edgar.lilienthal@khk.ee>
2019-09-23 11:30:52 +0300

    NHLstats  sorted by points top10

--- print/src/Example.java
+++ print/src/Example.java
@@ -1,23 +1,14 @@
 import java.util.Scanner;
+import nhlstats.NHLStatistics;
+
+
 
 public class Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type your name: ");
-        String firstName = reader.nextLine();
-        System.out.print("Type your age: ");
-        int firstAge = Integer.parseInt(reader.nextLine());
 
-        System.out.print("\n");
-
-        System.out.print("Type your name: ");
-        String secondtName = reader.nextLine();
-        System.out.print("Type your age: ");
-        int secondAge = Integer.parseInt(reader.nextLine());
-
-        System.out.print("\n");
-        int sum = firstAge + secondAge;
-
-        System.out.print(firstName + " and " + secondtName + " are " + sum + " years old in total." );
+        System.out.println("Top ten by points");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.top(10);
     }
 }
