917e0cbb34af3b148519c92b2f67335c03d3e65f
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-23 09:46:03 +0300

    Varying variables

--- print/src/Example.java
+++ print/src/Example.java
@@ -1,13 +1,14 @@
 public class Example {
     public static void main(String[] args) {
-        String text = "includes text";
-        int wholeNumber = 123;
-        double decimalNumber = 3.141592653;
-        boolean isTrue = true;
+        int chickens = 9000;
+        double bacon = 0.1;
+        String tractor = "Zetor";
 
-        System.out.println("The variable's type is text. Its value is " + text);
-        System.out.println("The variable's type is integer. Its value is "+ wholeNumber);
-        System.out.println("The variable's type is decimal number. Its value is "+ decimalNumber);
-        System.out.println("The variable's type is decimal truth value. Its value is "+ isTrue);
+
+        System.out.println("Chickens:\n" + chickens);
+        System.out.println("Bacon (kg)\n" + bacon);
+        System.out.println("A tractor:\n" + tractor + "\n");
+        System.out.println("In a nutshell:\n"  + chickens + "\n" + bacon + "\n" + tractor);
+
     }
 }
