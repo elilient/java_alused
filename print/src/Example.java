e58af4ba68a7977be26aa90672217491391e0a0a
Edgar Lilienthal <edgar.lilienthal@khk.ee>
2019-09-23 11:03:05 +0300

    Input name and age total

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,13 +3,21 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type a number: ");
-        int first = Integer.parseInt(reader.nextLine());
-        System.out.print("Type another number: ");
-        int second = Integer.parseInt(reader.nextLine());
-        System.out.print("\n");
-        int isHigher = Math.max(first, second);
+        System.out.print("Type your name: ");
+        String firstName = reader.nextLine();
+        System.out.print("Type your age: ");
+        int firstAge = Integer.parseInt(reader.nextLine());
 
-        System.out.print("The bigger number of the two numbers given was: " + isHigher);
+        System.out.print("\n");
+
+        System.out.print("Type your name: ");
+        String secondtName = reader.nextLine();
+        System.out.print("Type your age: ");
+        int secondAge = Integer.parseInt(reader.nextLine());
+
+        System.out.print("\n");
+        int sum = firstAge + secondAge;
+
+        System.out.print(firstName + " and " + secondtName + " are " + sum + " years old in total." );
     }
 }
