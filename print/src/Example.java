3dccdf352f599586b1ee67ca8c3bfce41c0ffde2
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-23 09:47:32 +0300

    Basic addition

--- print/src/Example.java
+++ print/src/Example.java
@@ -1,14 +1,8 @@
 public class Example {
     public static void main(String[] args) {
-        int chickens = 9000;
-        double bacon = 0.1;
-        String tractor = "Zetor";
-
-
-        System.out.println("Chickens:\n" + chickens);
-        System.out.println("Bacon (kg)\n" + bacon);
-        System.out.println("A tractor:\n" + tractor + "\n");
-        System.out.println("In a nutshell:\n"  + chickens + "\n" + bacon + "\n" + tractor);
-
+        int first = 2;
+        int second = 4;
+        int sum = first + second;
+        System.out.println(sum);
     }
 }
