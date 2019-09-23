b97b438ef6e9da02a92ceb166c53ae4b6e497df2
Edgar Lilienthal <edgar.lilienthal@khk.ee>
2019-09-23 10:59:07 +0300

    Input number higher

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type the radius: ");
-        int radius = Integer.parseInt(reader.nextLine());
+        System.out.print("Type a number: ");
+        int first = Integer.parseInt(reader.nextLine());
+        System.out.print("Type another number: ");
+        int second = Integer.parseInt(reader.nextLine());
         System.out.print("\n");
-        double pi = 3.14159265359;
-        double radiusCalc = 2 * pi * radius;
+        int isHigher = Math.max(first, second);
 
-        System.out.print("Circumference of the circle: " + radiusCalc);
+        System.out.print("The bigger number of the two numbers given was: " + isHigher);
     }
 }
