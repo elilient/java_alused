8e02801bbbcd2eb5e37208069244f428a4a0c682
Edgar Lilienthal <edgar.lilienthal@khk.ee>
2019-09-23 10:43:58 +0300

    Input numbers addition

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("What is your name? ");
-        String name = reader.nextLine();
+        System.out.print("Type a number ");
+        int first = Integer.parseInt(reader.nextLine());
+        System.out.print("Type another number: ");
+        int second = Integer.parseInt(reader.nextLine());
+        System.out.print("\n");
+        int sum = first + second;
 
-        System.out.println("Hi," + name);
+        System.out.print("Sum of the numbers: " + sum);
     }
 }
