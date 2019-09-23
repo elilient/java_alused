048da8961677c77e93466f4b883498358662c678
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-23 09:34:17 +0300

    Spruce

--- print/src/Example.java
+++ print/src/Example.java
@@ -1,5 +1,9 @@
 public class Example {
     public static void main(String[] args) {
-        System.out.print("Hello world!\n(And all the people of the world)");
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ****** ");
+        System.out.println("********");
+        System.out.println("   *   ");
     }
 }
