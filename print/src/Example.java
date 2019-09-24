0d5d3a0b2768834613aa97df9fb0aa1a47fdd9e0
Edgar <edgar.lilienthal@khk.ee>
2019-09-24 21:58:58 +0300

    Age check

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,22 +3,13 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) throws Exception {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type the points: ");
-        int points = Integer.parseInt(reader.nextLine());
+        System.out.print("How old are you?");
+        int age = Integer.parseInt(reader.nextLine());
 
-
-        if (points < 30) {
-            System.out.println("Grade: failed");
-        } else if (points < 35) {
-            System.out.println("Grade: 1");
-        } else if (points < 40) {
-            System.out.println("Grade: 2");
-        } else if (points < 45) {
-            System.out.println("Grade: 3");
-        } else if (points < 50) {
-            System.out.println("Grade: 4");
+        if (age > -1 && age < 121) {
+            System.out.println("OK");
         } else {
-            System.out.println("Grade: 5");
+            System.out.println("Impossible!");
         }
 
     }
