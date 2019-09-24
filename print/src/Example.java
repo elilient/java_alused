150fb88b493a317150fbfcca39fbc1d21eb98725
Edgar <edgar.lilienthal@khk.ee>
2019-09-24 21:47:51 +0300

    Even/odd numbers

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) throws Exception {
         Scanner reader = new Scanner(System.in);
-        System.out.print("How old are you? ");
+        System.out.print("Type a number: ");
         int number = Integer.parseInt(reader.nextLine());
 
-        if (number > 17) {
-            System.out.println("You have reached the age of majority!");
+        if (number % 2 == 1) {
+            System.out.println("Number " + number + " is odd.");
         }
 
-        if  (number < 18) {
-            System.out.println("You have not reached the age of majority yet!");
+        if  (number % 2 == 0) {
+            System.out.println("Number " + number + " is even.");
         }
 
     }
