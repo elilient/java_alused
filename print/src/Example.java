68e9103209fa0bf5604b5cfaa6b1513c71b268f8
Edgar <edgar.lilienthal@khk.ee>
2019-09-24 21:56:45 +0300

    Grades and points

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) throws Exception {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type the first number: ");
-        int firstNumber = Integer.parseInt(reader.nextLine());
-        System.out.print("Type the second number: ");
-        int secondNumber = Integer.parseInt(reader.nextLine());
+        System.out.print("Type the points: ");
+        int points = Integer.parseInt(reader.nextLine());
 
-        if (firstNumber > secondNumber) {
-            System.out.println("Greater number: " + firstNumber );
-        } else if (firstNumber == secondNumber) {
-            System.out.println("The numbers are equal!");
+
+        if (points < 30) {
+            System.out.println("Grade: failed");
+        } else if (points < 35) {
+            System.out.println("Grade: 1");
+        } else if (points < 40) {
+            System.out.println("Grade: 2");
+        } else if (points < 45) {
+            System.out.println("Grade: 3");
+        } else if (points < 50) {
+            System.out.println("Grade: 4");
         } else {
-            System.out.println("Greater number: " + secondNumber);
+            System.out.println("Grade: 5");
         }
 
     }
