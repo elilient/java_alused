f2d53a0e0e2dcc4cf36bede17edc6a0141818312
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-30 14:48:24 +0300

    Many prints

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        printText();
+        System.out.println("How many?");
+        int number = Integer.parseInt(reader.nextLine());
+
+
+        while (true) {
+            if (number > 0) {
+                printText();
+                number--;
+            } else {
+                break;
+            }
+        }
     }
 
     public static void printText() {
