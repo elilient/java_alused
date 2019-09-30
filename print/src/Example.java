27197545903ad604434d3f9936bac03f4002d2c5
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-30 11:12:15 +0300

    The sum of a set of numbers

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("First: ");
-        int first = Integer.parseInt(reader.nextLine());
-        System.out.print("Last: ");
-        int last = Integer.parseInt(reader.nextLine());
+        System.out.print("Until what? ");
+        int number = Integer.parseInt(reader.nextLine());
 
-
-        while (first != last+1) {
-            System.out.println(first);
-            first++;
+        int i = 0;
+        int result = 0;
+        while (i < number) {
+            i++;
+            result = result + i;
         }
+        System.out.println("Sum is " + result);
     }
 }
