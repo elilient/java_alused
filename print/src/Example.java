b08a5c476aceca1ff8cb9b5d22591b06c150e82c
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-30 10:28:28 +0300

    Sum of three numbers

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("What is your name? ");
-        String name = reader.nextLine();
+        int sum = 0;
+        int read;
 
-        System.out.println("Hi," + name);
+        System.out.println("Type the first number: ");
+        read = Integer.parseInt(reader.nextLine());
+        sum = read;
+        System.out.println("Type the Second number: ");
+        read = Integer.parseInt(reader.nextLine());
+        sum = sum + read;
+        System.out.println("Type the Third number: ");
+        read = Integer.parseInt(reader.nextLine());
+        sum = sum + read;
+
+        System.out.println(sum);
     }
 }
