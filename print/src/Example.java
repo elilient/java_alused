b8604fd9ff98a0bfdd5e4dea3ff5372dab9797da
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-30 11:25:08 +0300

    The sum between two numbers

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Until what? ");
-        int number = Integer.parseInt(reader.nextLine());
+        System.out.print("First: ");
+        int first = Integer.parseInt(reader.nextLine());
+        System.out.print("Second: ");
+        int second = Integer.parseInt(reader.nextLine());
 
-        int i = 0;
+
         int result = 0;
-        while (i < number) {
-            i++;
-            result = result + i;
+        while (first < second+1) {
+            result += first;
+            first++;
         }
         System.out.println("Sum is " + result);
     }
