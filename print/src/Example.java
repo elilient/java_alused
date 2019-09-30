0d89649ff3ec366aa99a95e8dd885bcfb4b91a65
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-30 10:48:41 +0300

    From hundred to one

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int number = 1;
-        while (number < 101) {
+        int number = 100;
+        while (number > 0) {
             System.out.println(number);
-            number++;
+            number--;
         }
     }
 }
