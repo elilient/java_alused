b97d0267ed8de46fb9cdaa48e3bb31d7f3d79502
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-30 10:57:34 +0300

    Up to a certain number

--- print/src/Example.java
+++ print/src/Example.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
+        System.out.print("Up to what number? ");
         Scanner reader = new Scanner(System.in);
-        int number = 2;
-        while (number < 101) {
-
+        int target = Integer.parseInt(reader.nextLine());
+        int number = 1;
+        while (number < target+1) {
             System.out.println(number);
-            number = number+2;
-
+            number++;
         }
     }
 }
