41a030fffa7b565dd2ddd2b24780bd440ed87dd1
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-30 14:34:38 +0300

    Summing and counting the numbers

--- print/src/Example.java
+++ print/src/Example.java
@@ -5,18 +5,20 @@ public class Example {
         Scanner reader = new Scanner(System.in);
         int number;
         int result = 0;
-
+        int count = 0;
+        System.out.println("Type a number: ");
         while (true) {
-            System.out.print("Type a number: ");
+
             number = Integer.parseInt(reader.nextLine());
 
             if (number != -1) {
+                count = count + 1;
                 result += number;
-                System.out.println("Number is: " + number);
 
             } else {
                 System.out.println("Thank you and see you later!");
                 System.out.println("The sum is " + result);
+                System.out.println("How many numbers: " + count);
                 break;
             }
 
