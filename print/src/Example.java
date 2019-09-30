4d91f00f1043a5818ee1ea7e26ef651f7bf32610
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-30 11:47:18 +0300

    Factorial

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("First: ");
-        int first = Integer.parseInt(reader.nextLine());
-        System.out.print("Second: ");
-        int second = Integer.parseInt(reader.nextLine());
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
 
+        int i = 1;
+        int result = 1;
+        int counter = 0;
+        while (i <= number) {
 
-        int result = 0;
-        while (first < second+1) {
-            result += first;
-            first++;
+            result = result * i;
+            i++;
+            System.out.println("Iteration: " + i + " number: " + number + "result: " + result);
         }
-        System.out.println("Sum is " + result);
+        System.out.println("Factorial is " + result);
     }
 }
