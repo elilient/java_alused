c24b53691fd26efac01159ad7a7d8638af00f8f0
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-30 14:30:42 +0300

    The sum of the numbers

--- print/src/Example.java
+++ print/src/Example.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type a number: ");
         int number;
-        number = Integer.parseInt(reader.nextLine());
+        int result = 0;
 
         while (true) {
-
+            System.out.print("Type a number: ");
+            number = Integer.parseInt(reader.nextLine());
 
             if (number != -1) {
-                number = Integer.parseInt(reader.nextLine());
+                result += number;
+                System.out.println("Number is: " + number);
+
             } else {
                 System.out.println("Thank you and see you later!");
+                System.out.println("The sum is " + result);
                 break;
             }
 
