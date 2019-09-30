f08a134ebc7cb8a4bbb350453d5753df165de4b9
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-09-30 14:40:46 +0300

    Even and odd numbers

--- print/src/Example.java
+++ print/src/Example.java
@@ -7,6 +7,8 @@ public class Example {
         int result = 0;
         int count = 0;
         float avg = 0;
+        int evenCounter = 0;
+        int oddCounter = 0;
         System.out.println("Type a number: ");
         while (true) {
 
@@ -16,11 +18,18 @@ public class Example {
                 count = count + 1;
                 result += number;
                 avg = (float)result/count;
+                if (number % 2 == 0) {
+                    evenCounter = evenCounter + 1;
+                } else {
+                    oddCounter = oddCounter +1;
+                }
             } else {
                 System.out.println("Thank you and see you later!");
                 System.out.println("The sum is " + result);
                 System.out.println("How many numbers: " + count);
                 System.out.println("Average: " + avg);
+                System.out.println("Even numbers: " + evenCounter);
+                System.out.println("Odd numbers: " + oddCounter);
                 break;
             }
 
