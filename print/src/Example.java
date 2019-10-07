cd01095dc68ae7ff8f1ec04e687a501e64f43d05
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-10-07 11:11:38 +0300

    Guessing a number game

--- print/src/Example.java
+++ print/src/Example.java
@@ -7,15 +7,19 @@ public class Example {
         Scanner reader = new Scanner(System.in);
         double randNumber = Math.random()*100;
         int intNumber = (int)randNumber;
-        System.out.println(intNumber);
 
         System.out.print("Guess a number: ");
         int number = Integer.parseInt(reader.nextLine());
         guesses++;
 
         while (true) {
-            if (number != intNumber) {
+            if (number > intNumber) {
                 System.out.println("The number is greater, guesses made: " + guesses);
+                System.out.print("Guess a number: ");
+                number = Integer.parseInt(reader.nextLine());
+                guesses++;
+            } else if (number < intNumber) {
+                System.out.println("The number is lesser, guesses made: " + guesses);
                 System.out.print("Guess a number: ");
                 number = Integer.parseInt(reader.nextLine());
                 guesses++;
