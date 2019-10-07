2cd484f84dcce5650e4b95edb8963bd21832286b
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-10-07 11:52:14 +0300

    The length of a name

--- print/src/Example.java
+++ print/src/Example.java
@@ -2,19 +2,10 @@ import java.util.Scanner;
 
 
 public class Example {
-    public static double average(int number1, int number2, int number3, int number4) {
-        int sum = sum(number1,number2,number3,number4);
-        double average = (double)sum/4;
-        return average;
-    }
-
-    public static int sum(int number1, int number2, int number3, int number4) {
-        int sum = number1 + number2 + number3 + number4;
-        return sum;
-    }
-
     public static void main(String[] args) {
-        double answer = average(4, 3, 6, 1);
-        System.out.println("average: " + answer);
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        System.out.print("Number of characters: " + name.length());
     }
 }
