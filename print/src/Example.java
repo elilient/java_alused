37f8b4792b64cfbdd79402fefe331a73b9d16ea4
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-10-07 12:37:58 +0300

    A word inside a word

--- print/src/Example.java
+++ print/src/Example.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type a word: ");
+        System.out.print("Type the first word: ");
         String word = reader.nextLine();
-        System.out.print("Length of the end part: ");
-        int number = Integer.parseInt(reader.nextLine());
+        System.out.print("Type the second word: ");
+        String word2 = reader.nextLine();
 
-        System.out.print("Result: " + word.substring(word.length()-number, word.length()));
+        int index = word.indexOf(word2);
 
 
+        if (index > 0) {
+            System.out.println("The word '" + word2 + " ' is found in the word '" + word +"'.");
+
+        } else {
+            System.out.println("The word '" + word2 + " ' is not found in the word '" + word +"'.");
+        }
 
 
     }
