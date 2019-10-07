56a81aaf0247c8d0cf191f83b764a7491d343294
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-10-07 11:58:47 +0300

    Seperating first characters with length fix

--- print/src/Example.java
+++ print/src/Example.java
@@ -6,12 +6,14 @@ public class Example {
         Scanner reader = new Scanner(System.in);
         System.out.print("Type your name: ");
         String name = reader.nextLine();
+        if (name.length()>2) {
+            char character1 = name.charAt(0);
+            char character2 = name.charAt(1);
+            char character3 = name.charAt(2);
+            System.out.println("1. character: " + character1);
+            System.out.println("2. character: " + character2);
+            System.out.println("3. character: " + character3);
+        }
 
-        char character1 = name.charAt(0);
-        char character2 = name.charAt(1);
-        char character3 = name.charAt(2);
-        System.out.println("1. character: " + character1);
-        System.out.println("2. character: " + character2);
-        System.out.println("3. character: " + character3);
     }
 }
