277da1ec0952aa6a081a5c7e185e0302673bd83b
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-10-07 12:04:34 +0300

    Separating characters

--- print/src/Example.java
+++ print/src/Example.java
@@ -6,13 +6,12 @@ public class Example {
         Scanner reader = new Scanner(System.in);
         System.out.print("Type your name: ");
         String name = reader.nextLine();
-        if (name.length()>2) {
-            char character1 = name.charAt(0);
-            char character2 = name.charAt(1);
-            char character3 = name.charAt(2);
-            System.out.println("1. character: " + character1);
-            System.out.println("2. character: " + character2);
-            System.out.println("3. character: " + character3);
+        int i = 0;
+        int count = 1;
+        while (i<name.length()) {
+            System.out.println(count+". character: " + name.charAt(i));
+            i++;
+            count++;
         }
 
     }
