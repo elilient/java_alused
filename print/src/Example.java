d9326d0c09436c1887363dd310a890b326f8f666
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-10-07 14:00:52 +0300

    Words

--- print/src/Example.java
+++ print/src/Example.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class Example {
-    public static String reverse(String text) {
-        String help = "";
-        int i = 1;
-        char character;
-        while (i<text.length()+1) {
-            int length = text.length()-i;
-            i++;
-            character = text.charAt(length);
-            help = help + character;
-        }
-        return help;
-    }
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>();
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type in your text: ");
-        String text = reader.nextLine();
-        System.out.println("In reverse order: " + reverse(text));
+        int i = 0;
+        String help = "";
+
+        while (true) {
+            System.out.print("Type a word: ");
+            String word = reader.nextLine();
+            if (word.isEmpty()) {
+                System.out.println(words.get(i));
+                i++;
+                break;
+            } else {
+                words.add(word);
+                help = help + word;
+            }
+
+        }
     }
 }
