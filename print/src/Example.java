a18e299e1f65203d33317369217a934a32ba85de
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-10-07 11:21:03 +0300

    Sum of Numbers

--- print/src/Example.java
+++ print/src/Example.java
@@ -1,33 +1,17 @@
 import java.util.Scanner;
 
+
 public class Example {
-    public static void main(String[] args) {
-        int guesses = 0;
-
-        Scanner reader = new Scanner(System.in);
-        double randNumber = Math.random()*100;
-        int intNumber = (int)randNumber;
-
-        System.out.print("Guess a number: ");
-        int number = Integer.parseInt(reader.nextLine());
-        guesses++;
-
-        while (true) {
-            if (number > intNumber) {
-                System.out.println("The number is greater, guesses made: " + guesses);
-                System.out.print("Guess a number: ");
-                number = Integer.parseInt(reader.nextLine());
-                guesses++;
-            } else if (number < intNumber) {
-                System.out.println("The number is lesser, guesses made: " + guesses);
-                System.out.print("Guess a number: ");
-                number = Integer.parseInt(reader.nextLine());
-                guesses++;
-            } else {
-                System.out.println("Congratulations, your guess is correct!");
-                break;
-            }
-        }
+    public static int sum(int number1, int number2, int number3, int number4) {
+        int sum = number1 + number2 + number3 + number4;
+        return sum;
     }
+    public static void main(String[] args) {
+        int answer = sum(4, 3, 6, 1);
+        System.out.println("sum: " + answer);
+    }
+
+
+
 
 }
