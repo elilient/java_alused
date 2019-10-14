7a08f489c42d18f0c7f64f4b6e83522cc9c4250b
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-10-14 14:10:17 +0300

    Is the number more than once in the list?

--- print/src/Example.java
+++ print/src/Example.java
@@ -1,53 +1,36 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
         list.add(3);
         list.add(2);
         list.add(7);
         list.add(2);
 
-        System.out.println("The greatest number is: " + variance(list));
-    }
-
-    public static int greatest(ArrayList<Integer> list) {
-        ArrayList<Integer> numbers = new ArrayList<>();
-        for (int number : list) {
-            numbers.add(number);
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        if (moreThanOnce(list, number)) {
+            System.out.println(number + " appears more than once.");
+        } else {
+            System.out.println(number + "does not appear more than once.");
         }
-        return Collections.max(numbers);
     }
+    public static boolean moreThanOnce(ArrayList<Integer> list, int number) {
+        int count = 0;
+        for (int i : list) {
+            if (i == number) {
+                count++;
+            }
 
-    public static int sum(ArrayList<Integer> list) {
-        int sum = 0;
-
-        for (int number : list) {
-            sum += number;
         }
-        return sum;
-    }
-
-    public static double average(ArrayList<Integer> list) {
-        double average = 0;
-        int i = 0;
-        for (int number : list) {
-            i++;
+        if (count>1) {
+            return true;
+        } else {
+            return false;
         }
-        average = (double)sum(list) / i;
-        return average;
-    }
-
-    public static double variance(ArrayList<Integer> list) {
-        double variance = 0;
-        int i = 0;
-        double answer = 0;
-        for (int number : list) {
-            variance += Math.pow(number-average(list),2);
-            i++;
-        }
-        answer = variance/(i-1);
-        return answer;
     }
 }
