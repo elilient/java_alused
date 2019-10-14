cd7ad5bfb1c23d9afe889fcb23fcb0c222d7aa4a
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-10-14 13:56:30 +0300

    Variance

--- print/src/Example.java
+++ print/src/Example.java
@@ -9,7 +9,7 @@ public class Example {
         list.add(7);
         list.add(2);
 
-        System.out.println("The greatest number is: " + greatest(list));
+        System.out.println("The greatest number is: " + variance(list));
     }
 
     public static int greatest(ArrayList<Integer> list) {
@@ -18,5 +18,36 @@ public class Example {
             numbers.add(number);
         }
         return Collections.max(numbers);
+    }
+
+    public static int sum(ArrayList<Integer> list) {
+        int sum = 0;
+
+        for (int number : list) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public static double average(ArrayList<Integer> list) {
+        double average = 0;
+        int i = 0;
+        for (int number : list) {
+            i++;
+        }
+        average = (double)sum(list) / i;
+        return average;
+    }
+
+    public static double variance(ArrayList<Integer> list) {
+        double variance = 0;
+        int i = 0;
+        double answer = 0;
+        for (int number : list) {
+            variance += Math.pow(number-average(list),2);
+            i++;
+        }
+        answer = variance/(i-1);
+        return answer;
     }
 }
