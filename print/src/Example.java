62482a98f0a656e7648f1319feb1b9697401786a
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-10-14 14:45:46 +0300

    Smart combining

--- print/src/Example.java
+++ print/src/Example.java
@@ -5,14 +5,15 @@ public class Example {
         ArrayList<Integer> list1 = new ArrayList<>();
         ArrayList<Integer> list2 = new ArrayList<>();
 
-        list1.add(4);
+        list1.add(5);
         list1.add(3);
 
         list2.add(5);
         list2.add(10);
         list2.add(7);
 
-        combine(list1, list2);
+        //combine(list1, list2);
+        smartCombine(list1, list2);
 
         System.out.println(list1);
 
@@ -21,6 +22,16 @@ public class Example {
 
     public static void combine(ArrayList<Integer> first, ArrayList<Integer> second) {
         first.addAll(second);
+    }
+
+    public static void smartCombine(ArrayList<Integer> first, ArrayList<Integer> second) {
+        for (int i : second) {
+            if (first.contains(i)) {
+
+            } else {
+                first.add(i);
+            }
+        }
     }
 
 }
