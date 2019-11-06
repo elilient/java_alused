d3ebc99641a4bde0e44b1c825a1baa14fd334a4e
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-11-06 09:09:38 +0200

    E84: Overloaded counter

--- print/src/Program.java
+++ print/src/Program.java
@@ -1,15 +1,13 @@
-import java.util.ArrayList;
 
 public class Program {
     public static void main(String[] args) {
+        Counter count = new Counter(10, true);
+        System.out.println(count.value());
+        count.decrease();
+        System.out.println(count.value());
+        count.increase();
+        System.out.println(count.value());
 
-        LotteryNumbers lotteryNumbers = new LotteryNumbers();
-        ArrayList<Integer> numbers = lotteryNumbers.numbers();
 
-        System.out.println("Lottery numbers:");
-        for (int number : numbers) {
-            System.out.print(number + " ");
-        }
-        System.out.println("");
     }
 }
